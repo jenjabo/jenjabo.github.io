2c1886ffac2b67cd1feec6e98f7904d92bba3232
artofemily <150404823+artofemily@users.noreply.github.com>
2023-11-15 19:51:43 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function showType(fileInput) {
+  const files = fileInput.files;
 
+  for (const i = 0; i < files.length; i++) {
+    const name = files[i].name;
+    const type = files[i].type;
+    alert('Filename: ' + name + ' , Type: ' + type);
+  }
+}
