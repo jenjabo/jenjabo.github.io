1c3f51e60c61b096c73f3c0026f013a8c745aa8d
jenjabo <145815189+jenjabo@users.noreply.github.com>
2023-11-15 20:08:13 -0800

    Update file.php

--- file.php
+++ file.php
@@ -3,12 +3,10 @@ file_uploads = On
 <!DOCTYPE html>
 <html>
   <body>
-
     <form action="upload.php" method="post" enctype="multipart/form-data">
       Select file to upload:
       <input type="file" name="fileToUpload" id="fileToUpload">
       <input type="submit" value="Upload CSV" name="submit">
     </form>
-  
   </body>
 </html>
